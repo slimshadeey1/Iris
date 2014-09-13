b1f9c4e7e19c27d6a4446e6470cbd725d6576e1f
Slimshadeey1 <slimshadeey1@gmail.com>
2014-09-13 01:22:26 -0700

    Update WeightedRandom.java

--- src/main/java/Iris/Math/WeightedRandom.java
+++ src/main/java/Iris/Math/WeightedRandom.java
@@ -25,7 +25,7 @@ public class WeightedRandom {
         if (ExecList.contains(clazz)) {
             return false;
         } else {
-            sumRun = sumRun + clazz.getFreququency();
+            sumRun = sumRun + clazz.getFrequency();
             ExecList.add(clazz);
             return true;
         }
@@ -35,7 +35,7 @@ public class WeightedRandom {
         if (returnList.isEmpty()) {
             return false;
         } else {
-            sumReturn = sumReturn + e.getFreququency();
+            sumReturn = sumReturn + e.getFrequency();
             returnList.add(e);
             return true;
         }
@@ -49,9 +49,9 @@ public class WeightedRandom {
             ArrayList<IWeightedRunnable> clazzes = new ArrayList<IWeightedRunnable>();
             int rand = random.nextInt(sumRun);
             for (IWeightedRunnable clazz : ExecList) {     // For each item on the list
-                if (rand < clazz.getFreququency()) {               // This checks the items set frequency to the current frequency, then adds it to the qualification list
+                if (rand < clazz.getFrequency()) {               // This checks the items set frequency to the current frequency, then adds it to the qualification list
                     if (rand <= FINE_CURVE) {                  // This adds a curve to the generation of the drops list, for items under the curve.
-                        if (clazz.getFreququency() <= FINE_CURVE)  // the reason for this being if something is 1% and we get 1%, then everything above
+                        if (clazz.getFrequency() <= FINE_CURVE)  // the reason for this being if something is 1% and we get 1%, then everything above
                             clazzes.add(clazz);                // would be added making it nearly impossible to get. This just balances it a little more
                     } else {                                   // We will only add the items the are below 10% but greater than the cutoff
                         clazzes.add(clazz);                    // Adding everything to list if above 10%, and meets the cutoff
@@ -72,9 +72,9 @@ public class WeightedRandom {
             ArrayList<IWeightedObject> objects = new ArrayList<IWeightedObject>();
             int rand = random.nextInt(sumRun);
             for (IWeightedObject e : returnList) {     // For each item on the list
-                if (rand < e.getFreququency()) {               // This checks the items set frequency to the current frequency, then adds it to the qualification list
+                if (rand < e.getFrequency()) {               // This checks the items set frequency to the current frequency, then adds it to the qualification list
                     if (rand <= FINE_CURVE) {                  // This adds a curve to the generation of the drops list, for items under the curve.
-                        if (e.getFreququency() <= FINE_CURVE)  // the reason for this being if something is 1% and we get 1%, then everything above
+                        if (e.getFrequency() <= FINE_CURVE)  // the reason for this being if something is 1% and we get 1%, then everything above
                             objects.add(e);                // would be added making it nearly impossible to get. This just balances it a little more
                     } else {                                   // We will only add the items the are below 10% but greater than the cutoff
                         objects.add(e);                    // Adding everything to list if above 10%, and meets the cutoff
