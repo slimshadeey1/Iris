1deb3cdd782e630b9febf2020eff41e288f4775d
slimshadeey1 <slimshadeey1@gmail.com>
2014-09-13 00:33:05 -0700

    Cleanup

--- src/main/java/Iris/Forge/ModelRenderTMT/TransformGroup.java
+++
@@ -1,9 +0,0 @@
-package Iris.Forge.ModelRenderTMT;
-
-import net.minecraft.util.*;
-
-public abstract class TransformGroup
-{
-	public abstract double getWeight();
-	public abstract Vec3 doTransformation(PositionTransformVertex vertex);
-}
