ffc4fe61664717a1414e1f0af02d6348ecc7e26c
slimshadeey1 <slimshadeey1@gmail.com>
2014-09-13 00:40:58 -0700

    Re-Organization

---
+++ src/main/java/Iris/Math/API/WeightedRunnable.java
@@ -0,0 +1,21 @@
+package Iris.Math.API;
+
+import Iris.Interfaces.*;
+
+/**
+ * Created by Ben Byers on 9/13/2014.
+ */
+public abstract class WeightedRunnable implements IWeightedRunnable {
+    private Integer Freququency;
+
+    @Override
+    public void setFreququency(Integer freququency) {
+        Freququency = freququency;
+    }
+
+    @Override
+    public Integer getFreququency() {
+        return Freququency;
+    }
+
+}
