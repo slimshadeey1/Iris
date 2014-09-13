c737f968d517f6b307f0c6936e5336eb5977f35d
Slimshadeey1 <slimshadeey1@gmail.com>
2014-09-13 01:23:28 -0700

    Update IWeightedObject.java

--- src/main/java/Iris/Interfaces/IWeightedObject.java
+++ src/main/java/Iris/Interfaces/IWeightedObject.java
@@ -7,6 +7,6 @@ import Iris.*;
  * Created by Ben Byers on 9/10/2014.
  */
 public interface IWeightedObject {
-    public Integer getFreququency();
-    public void setFreququency(Integer freququency);
+    public Integer getFrequency();
+    public void setFrequency(Integer freququency);
 }
