0a02ae450ae22eede7dcd58bd18164c65852e0dd
Ben Byers <slimshadeey1@gmail.com>
2015-03-20 14:25:17 -0700

    Another

---
+++ src/main/java/com/benbyers/Iris/math/units/electrical/Siemens.java
@@ -0,0 +1,18 @@
+package com.benbyers.Iris.math.units.electrical;
+
+import com.benbyers.Iris.math.units.Metric.BaseMetric;
+import com.benbyers.Iris.math.units.Metric.MetricConstants;
+
+/**
+ * Created by benbyers on 3/11/15.
+ */
+public class Siemens extends BaseMetric {
+    public Siemens(double value) {
+        super(value);
+    }
+
+    @Override public int thisValue() {
+        return MetricConstants.Base.getFactor();
+    }
+
+}
---
+++ src/main/java/com/benbyers/Iris/math/units/electrical/Volt.java
@@ -0,0 +1,16 @@
+package com.benbyers.Iris.math.units.electrical;
+
+import com.benbyers.Iris.math.units.Metric.BaseMetric;
+import com.benbyers.Iris.math.units.Metric.MetricConstants;
+
+/**
+ * Created by benbyers on 3/11/15.
+ */
+public class Volt extends BaseMetric {
+    public Volt(double value) {
+        super(value);
+    }
+    @Override public int thisValue() {
+        return MetricConstants.Base.getFactor();
+    }
+}
